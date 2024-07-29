72423481192d89ca87b262c9c90ec226dba5861f
madeehawaqas04 <124630346+madeehawaqas04@users.noreply.github.com>
2024-07-29 22:12:52 +0500

    Add project files.

---
+++ NYCHospital/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NYCHospital.Models;
+
+namespace NYCHospital.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
+
---
+++ NYCHospital/Models/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NYCHospital.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        public string Address { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
